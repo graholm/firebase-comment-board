9a9d4d58ccdb65bdc4bb203d7f2aa8bcc42d9c0a
graholm <graholm@nuevaschool.org>
2016-11-23 08:43:40 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var commentInput = $("#comment-input")
-var commentDisplay = $(".not-actually-the-right-class")
+var input = $("#new-item-input")
+var commentDisplay = $("comment-display")
 
 commentInput.keydown(function(e) {
   if (e.which == 13) {
@@ -10,3 +10,18 @@ commentInput.keydown(function(e) {
 onNewListItem("whats-my-name-again", function(newComment) {
   commentDisplay.append("<span>" + newComment + "</span>")
 })
+
+
+
+input.keydown(function(e) {	
+  if (e.keyCode == "13") {
+    var valueToSave = input.val();
+    addListItem("graceList", valueToSave);
+  }
+})
+
+
+onNewListItem("graceList", function(value) {
+  $("#list-display").append("<div>" + value +"</div>")
+
+}) 
